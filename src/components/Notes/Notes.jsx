3917bee9b4aeffcefa8c6a22e764d288280cdb91
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-10-12 17:35:35 +0200

    add notes container

---
+++ src/components/Notes/Notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notes = () => {
+  return (
+    <>
+      <div class="notes">{/* <!-- dynamically added notes --> */}</div>
+    </>
+  );
+};
+
+export default Notes;
