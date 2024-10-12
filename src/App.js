ee1141009391cd424d617a1d88930a86641bda07
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-10-12 16:34:41 +0200

    add global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./components/Tooltip/Tooltip.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
