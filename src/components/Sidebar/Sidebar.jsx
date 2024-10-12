4d54b2111b83c88a819c5498e4035f4d54dd6232
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-10-12 17:05:05 +0200

    change 'class' to 'className'

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,26 +3,28 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div>
-      <div class="sidebar">
-        <div class="sidebar-item active-item">
-          <span class="material-icons-outlined hover active">lightbulb</span>
-          <span class="sidebar-text">Notes</span>
+      <div className="sidebar">
+        <div className="sidebar-item active-item">
+          <span className="material-icons-outlined hover active">
+            lightbulb
+          </span>
+          <span className="sidebar-text">Notes</span>
         </div>
-        <div class="sidebar-item">
-          <span class="material-icons-outlined hover">notifications</span>
-          <span class="sidebar-text">Reminders</span>
+        <div className="sidebar-item">
+          <span className="material-icons-outlined hover">notifications</span>
+          <span className="sidebar-text">Reminders</span>
         </div>
-        <div class="sidebar-item">
-          <span class="material-icons-outlined hover">edit</span>
-          <span class="sidebar-text">Edit Labels</span>
+        <div className="sidebar-item">
+          <span className="material-icons-outlined hover">edit</span>
+          <span className="sidebar-text">Edit Labels</span>
         </div>
-        <div class="sidebar-item">
-          <span class="material-icons-outlined hover">archive</span>
-          <span class="sidebar-text">Archive</span>
+        <div className="sidebar-item">
+          <span className="material-icons-outlined hover">archive</span>
+          <span className="sidebar-text">Archive</span>
         </div>
-        <div class="sidebar-item">
-          <span class="material-icons-outlined hover">delete</span>
-          <span class="sidebar-text">Trash</span>
+        <div className="sidebar-item">
+          <span className="material-icons-outlined hover">delete</span>
+          <span className="sidebar-text">Trash</span>
         </div>
       </div>
     </div>
