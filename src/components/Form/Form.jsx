c9804ceb3929abd7487cd7a2cac5d9c681678027
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-10-12 17:22:27 +0200

    add tooltip to forms

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,99 +1,60 @@
 import React from "react";
 import "./Form.css";
+import Tooltip from "../Tooltip/Tooltip";
 
 const Form = () => {
   return (
-    <div>
-      <div class="form-container inactive-form">
+    <>
+      {/* Inactive Form */}
+      <div className="form-container inactive-form">
         <form>
-          <input class="note-text" type="text" placeholder="Take a note..." />
-          <div class="form-actions">
-            <div class="tooltip">
-              <span class="material-icons-outlined hover">check_box</span>
-              <span class="tooltip-text">New List</span>
-            </div>
-            <div class="tooltip">
-              <span class="material-icons-outlined hover">brush</span>
-              <span class="tooltip-text">New Drawing</span>
-            </div>
-            <div class="tooltip">
-              <span class="material-icons-outlined hover">image</span>
-              <span class="tooltip-text">New Image</span>
-            </div>
+          <input
+            className="note-text"
+            type="text"
+            placeholder="Take a note..."
+          />
+          <div className="form-actions">
+            <Tooltip icon="check_box" text="New List" />
+
+            <Tooltip icon="brush" text="New Drawing" />
+            <Tooltip icon="image" text="New Image" />
           </div>
         </form>
       </div>
 
-      <div class="form-container active-form">
-        <form class="form" id="form">
+      {/* Active Form */}
+      <div className="form-container active-form">
+        <form className="form" id="form">
           <input
             id="note-title"
             type="text"
-            class="note-title"
+            className="note-title"
             placeholder="Title"
           />
           <input
             id="note-text"
-            class="note-text"
+            className="note-text"
             type="text"
             placeholder="Take a note..."
           />
-          <div class="form-actions">
-            <div class="icons">
-              <div class="tooltip">
-                <span class="material-icons-outlined hover small-icon">
-                  add_alert
-                </span>
-                <span class="tooltip-text">Remind me</span>
-              </div>
-              <div class="tooltip">
-                <span class="material-icons-outlined hover small-icon">
-                  person_add
-                </span>
-                <span class="tooltip-text">Collaborator</span>
-              </div>
-              <div class="tooltip">
-                <span class="material-icons-outlined hover small-icon">
-                  palette
-                </span>
-                <span class="tooltip-text">Change Color</span>
-              </div>
-              <div class="tooltip">
-                <span class="material-icons-outlined hover small-icon">
-                  image
-                </span>
-                <span class="tooltip-text">Add Image</span>
-              </div>
-              <div class="tooltip">
-                <span class="material-icons-outlined hover small-icon">
-                  archive
-                </span>
-                <span class="tooltip-text">Archive</span>
-              </div>
-              <div class="tooltip">
-                <span class="material-icons-outlined hover small-icon">
-                  more_vert
-                </span>
-                <span class="tooltip-text">More</span>
-              </div>
-              <div class="tooltip">
-                <span class="material-icons-outlined hover small-icon">
-                  undo
-                </span>
-                <span class="tooltip-text">Undo</span>
-              </div>
-              <div class="tooltip">
-                <span class="material-icons-outlined hover small-icon">
-                  redo
-                </span>
-                <span class="tooltip-text">Redo</span>
-              </div>
+          <div className="form-actions">
+            <div className="icons">
+              <Tooltip icon="add_alert" text="Remind me" />
+              <Tooltip icon="person_add" text="Collaborator" />
+              <Tooltip icon="palette" text="Change Color" />
+              <Tooltip icon="image" text="Add Image" />
+              <Tooltip icon="archive" text="Archive" />
+              <Tooltip icon="more_vert" text="More" />
+              <Tooltip icon="undo" text="Undo" />
+              <Tooltip icon="redo" text="Redo" />
             </div>
-            <button class="close-btn">Close</button>
+            <button type="button" className="close-btn">
+              Close
+            </button>
           </div>
         </form>
       </div>
-    </div>
+    </>
   );
 };
 
