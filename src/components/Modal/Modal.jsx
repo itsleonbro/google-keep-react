abc522235e2f609aec00afe11b4164aff5db2416
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-16 01:16:55 +0200

    use delete save functions

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,46 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Modal.css";
 import Tooltip from "../Tooltip/Tooltip";
 
-const Modal = () => {
+const Modal = ({ note, onClose, onSave, isModalOpen, onDelete }) => {
+  const [title, setTitle] = useState(note.title || "");
+  const [text, setText] = useState(note.text || "");
+
+  useEffect(() => {
+    setTitle(note.title);
+    setText(note.text);
+  }, [note]);
+
+  const handleSave = () => {
+    onSave(note.id, title, text);
+  };
+
   return (
-    <>
-      <div className="modal">
-        <div className="modal-content">
-          <div className="form-container">
-            <form className="form" id="modal-form">
-              <input
-                id="modal-title"
-                type="text"
-                className="note-title"
-                placeholder="Title"
-              />
-              <input
-                id="modal-text"
-                className="note-text"
-                type="text"
-                placeholder="Take a note..."
-              />
-              <div className="form-actions">
-                <div className="icons">
-                  <Tooltip icon="add_alert" text="Remind me" />
-                  <Tooltip icon="person_add" text="Collaborator" />
-                  <Tooltip icon="palette" text="Change Color" />
-                  <Tooltip icon="image" text="Add Image" />
-                  <Tooltip icon="archive" text="Archive" />
-                  <Tooltip icon="more_vert" text="More" />
-                  <Tooltip icon="undo" text="Undo" />
-                  <Tooltip icon="redo" text="Redo" />
-                </div>
-                <button type="button" className="close-btn" id="modal-btn">
-                  Close
-                </button>
-              </div>
-            </form>
+    <div className={`modal ${isModalOpen ? "open-modal" : ""}`}>
+      <div className="form-container">
+        <form className="form" id="modal-form">
+          <input
+            id="modal-title"
+            type="text"
+            className="note-title"
+            placeholder="Title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <input
+            id="modal-text"
+            className="note-text"
+            type="text"
+            placeholder="Take a note..."
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+          <div className="form-actions">
+            <Tooltip icon="add_alert" text="Remind me" />
+            <Tooltip icon="person_add" text="Collaborator" />
+            <Tooltip icon="palette" text="Change Color" />
+            <Tooltip icon="image" text="Add Image" />
+            <Tooltip
+              icon="archive"
+              text="Archive"
+              onClick={e => {
+                e.stopPropagation();
+                onDelete();
+                onClose();
+              }}
+            />
+            <Tooltip icon="more_vert" text="More" />
+            <button
+              type="button"
+              onClick={() => {
+                handleSave();
+                onClose();
+              }}
+            >
+              Close
+            </button>
           </div>
-        </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
